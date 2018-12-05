7e4196765803129f026e8abc6c0e17893d07c8bd
shadaileng <qpf0510@qq.com>
2018-12-05 23:25:15 +0800

    pom.xml

--- embed-tomcat/src/main/java/com/qpf/Main.java
+++ embed-tomcat/src/main/java/com/qpf/Main.java
@@ -1,8 +1,19 @@
 package com.qpf;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(System.getProperty("user.dir"));
 //        WebService.webService().start();
+        Main main = new Main();
+        main.show();
+    }
+    public void show() {
+
+        System.out.println(System.getProperty("user.dir"));
+        System.out.println(getClass());
+        System.out.println(getClass().getResource("/"));
+        System.out.println(getClass().getResource("/").getPath());
+        System.out.println(new File(getClass().getResource("/").getPath()));
     }
 }
