f72198878fe6ed2139c0852cafa5b50ef17f6d2b
shadaileng <qpf0510@qq.com>
2018-12-07 16:41:40 +0800

    startup

--- embed-tomcat/src/main/java/com/qpf/Main.java
+++ embed-tomcat/src/main/java/com/qpf/Main.java
@@ -1,18 +1,27 @@
 package com.qpf;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 //        WebService.webService().start();
         Main main = new Main();
         main.show();
     }
-    public void show() {
+    public void show() throws IOException {
 
+//        File tempFile = File.createTempFile("tomcat.", ".8080");
+//        tempFile.delete();
+//        tempFile.mkdir();
+//        tempFile.deleteOnExit();
+//        System.out.println(tempFile.getAbsolutePath());
+        System.out.println(getClass().getResource("").getFile());
+        System.out.println(getClass().getResource("/index.html"));
         System.out.println(System.getProperty("user.dir"));
+        System.out.println(ClassLoader.getSystemResource("."));
         System.out.println(getClass());
-        System.out.println(getClass().getResource("/"));
+        System.out.println(this.getClass().getResource("/"));
         System.out.println(getClass().getResource("/").getPath());
         System.out.println(new File(getClass().getResource("/").getPath()));
     }
--- embed-tomcat/src/main/java/com/qpf/WebService.java
+++ embed-tomcat/src/main/java/com/qpf/WebService.java
@@ -1,17 +1,21 @@
 package com.qpf;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.Host;
 import org.apache.catalina.Server;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.core.AprLifecycleListener;
+import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
 
 import java.io.File;
 
 public class WebService {
     private String hostName="localhost";
     private int port = 8090;
-    private String root = "web";
+    private String root = "embed-tomcat/src/main/public";
 
     private int maxPostSize = 0;
     private int maxThreads = 200;
@@ -46,13 +50,23 @@ public class WebService {
             tomcat = new Tomcat();
             tomcat.setPort(this.port);
             tomcat.setHostname(hostName);
-            tomcat.setBaseDir(".");
+            File tempFile = File.createTempFile("tomcat.", ".8080");
+            tempFile.delete();
+            tempFile.mkdir();
+            tempFile.deleteOnExit();
+            tomcat.setBaseDir(tempFile.getAbsolutePath());
 
             configServer(tomcat.getServer());
             configHost(tomcat.getHost());
             configConnector(tomcat.getConnector());
 
-            tomcat.addWebapp("", System.getProperty("user.dir") + File.separator + root);
+            StandardContext context = (StandardContext) tomcat.addWebapp("/", getClass().getResource("").getPath());
+//            StandardContext context = (StandardContext) tomcat.addWebapp("/", new File(root).getAbsolutePath());
+            File webInfoClasses = new File("target/classes");
+//            StandardRoot root = new StandardRoot(context);
+//            root.addPreResources(new DirResourceSet(root, "/WEB-INF/classes", webInfoClasses.getAbsolutePath(), "/"));
+//            context.setResources(root);
+//            tomcat.addWebapp("/", System.getProperty("user.dir") + File.separator + root);
 
             tomcat.start();
             tomcat.getServer().await();
