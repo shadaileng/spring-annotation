507448365fcc44408aa88c1df1c5def2175b9090
shadaileng <qpf0510@qq.com>
2018-12-09 11:01:06 +0800

    打包外置依赖

--- embed-tomcat/src/main/java/com/qpf/Main.java
+++ embed-tomcat/src/main/java/com/qpf/Main.java
@@ -5,9 +5,9 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        WebService.webService().start();
-        Main main = new Main();
-        main.show();
+        WebService.webService().start();
+//        Main main = new Main();
+//        main.show();
     }
     public void show() throws IOException {
 
@@ -16,13 +16,20 @@ public class Main {
 //        tempFile.mkdir();
 //        tempFile.deleteOnExit();
 //        System.out.println(tempFile.getAbsolutePath());
+        System.out.println(System.getProperty("java.io.tmpdir"));
         System.out.println(getClass().getResource("").getFile());
-        System.out.println(getClass().getResource("/index.html"));
+        System.out.println(getClass().getResource("/public/index.html"));
         System.out.println(System.getProperty("user.dir"));
         System.out.println(ClassLoader.getSystemResource("."));
         System.out.println(getClass());
-        System.out.println(this.getClass().getResource("/"));
-        System.out.println(getClass().getResource("/").getPath());
-        System.out.println(new File(getClass().getResource("/").getPath()));
+        System.out.println(this.getClass().getResource("/public/index.html"));
+        System.out.println(getClass().getResource("/") == null ? "null" : getClass().getResource("/").getPath());
+        System.out.println(new File(getClass().getResource("/") == null ? "." : getClass().getResource("/").getPath()));
+        String path = this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
+        System.out.println(path);
+        path = this.getClass().getProtectionDomain().getCodeSource().getLocation().getFile();
+        System.out.println(path);
+        path = System.getProperty("java.class.path");
+        System.out.println(path);
     }
 }
--- embed-tomcat/src/main/java/com/qpf/WebService.java
+++ embed-tomcat/src/main/java/com/qpf/WebService.java
@@ -1,29 +1,97 @@
 package com.qpf;
 
-import org.apache.catalina.Context;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 import org.apache.catalina.Host;
 import org.apache.catalina.Server;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.core.AprLifecycleListener;
-import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
-import org.apache.catalina.webresources.DirResourceSet;
-import org.apache.catalina.webresources.StandardRoot;
+import org.apache.commons.lang.StringUtils;
 
 import java.io.File;
+import java.io.IOException;
 
 public class WebService {
-    private String hostName="localhost";
-    private int port = 8090;
-    private String root = "embed-tomcat/src/main/public";
 
-    private int maxPostSize = 0;
-    private int maxThreads = 200;
-    private int acceptCount = 100;
+    @AllArgsConstructor // 全参构造方法
+    @NoArgsConstructor  // 无参构造方法
+    @Data               // getter setter
+    @Accessors(chain=true) //链式访问
+    public static class Builder {
 
-    private Tomcat tomcat;
+        private String hostName="localhost";
+        private String baseDir = new File(System.getProperty("java.io.tmpdir")).getAbsolutePath();
+        private int port = 8090;
+        private String protocol = "org.apache.coyote.http11.Http11NioProtocol";
+        private String uriEncoding = "UTF-8";
+//        private String root = "embed-tomcat/src/main/resources";
+        String path_ = getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
+        private String root = path_.substring(0, path_.lastIndexOf(File.separator) + 1) + File.separator + "public";
 
-    public WebService() {}
+        private int maxPostSize = 0;
+        private int maxThreads = 200;
+        private int acceptCount = 100;
+        public WebService build() {
+            if (StringUtils.isEmpty(root)) {
+                throw new IllegalArgumentException(StringUtils.replace("root is Empty", "root", root));
+            }
+            File rootDir = new File(root);
+            if (!rootDir.isDirectory()) {
+                throw new IllegalArgumentException(StringUtils.replace("root is not directory", "root", root));
+            }
+            if (!rootDir.exists()) {
+                throw new IllegalArgumentException(StringUtils.replace("root is not exists", "root", root));
+            }
+
+            return new WebService(this);
+        }
+    }
+
+//    private Tomcat tomcat;
+    private Builder builder;
+
+    public WebService(Builder builder) {
+        this.builder = builder;
+        initServer();
+    }
+
+    private void initServer() {
+        updateProperties();
+        try {
+            // 1. 创建内嵌Tomcat
+            Tomcat tomcat = new Tomcat();
+            // 2. 设置端口
+            tomcat.setPort(builder.port);
+            // 设置主机
+            tomcat.setHostname(builder.hostName);
+
+            // 设置workspace
+            tomcat.setBaseDir(getWorkspace());
+
+            configServer(tomcat.getServer());
+            configHost(tomcat.getHost());
+            configConnector(tomcat.getConnector());
+
+
+            String path = new File(builder.root).getAbsolutePath();
+            System.out.println(path);
+            tomcat.addWebapp("/", path);
+//            StandardContext context = (StandardContext) tomcat.addWebapp("/", new File(root).getAbsolutePath());
+//            File webInfoClasses = new File("target/classes");
+//            StandardRoot root = new StandardRoot(context);
+//            root.addPreResources(new DirResourceSet(root, "/WEB-INF/classes", webInfoClasses.getAbsolutePath(), "/"));
+//            context.setResources(root);
+//            tomcat.addWebapp("/", System.getProperty("user.dir") + File.separator + root);
+
+            tomcat.start();
+            tomcat.getServer().await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     private void updateProperties() {
 
@@ -34,50 +102,36 @@ public class WebService {
     }
     private void configHost(Host host) {
         host.setAppBase(System.getProperty("user.dir"));
+        host.setAutoDeploy(false);
     }
     private void configConnector(Connector connector) {
         connector.setURIEncoding("UTF-8");
-        connector.setMaxPostSize(maxPostSize);
-        connector.setAttribute("maxThreads", maxThreads);
-        connector.setAttribute("acceptCount", acceptCount);
+        connector.setMaxPostSize(builder.maxPostSize);
+        connector.setAttribute("maxThreads", builder.maxThreads);
+        connector.setAttribute("acceptCount", builder.acceptCount);
         connector.setAttribute("disableUploadTimeout", Boolean.valueOf(true));
         connector.setAttribute("enableLookups", Boolean.valueOf(false));
     }
 
     public void start() {
-        updateProperties();
-        try {
-            tomcat = new Tomcat();
-            tomcat.setPort(this.port);
-            tomcat.setHostname(hostName);
-            File tempFile = File.createTempFile("tomcat.", ".8080");
-            tempFile.delete();
-            tempFile.mkdir();
-            tempFile.deleteOnExit();
-            tomcat.setBaseDir(tempFile.getAbsolutePath());
 
-            configServer(tomcat.getServer());
-            configHost(tomcat.getHost());
-            configConnector(tomcat.getConnector());
+    }
 
-            StandardContext context = (StandardContext) tomcat.addWebapp("/", getClass().getResource("").getPath());
-//            StandardContext context = (StandardContext) tomcat.addWebapp("/", new File(root).getAbsolutePath());
-            File webInfoClasses = new File("target/classes");
-//            StandardRoot root = new StandardRoot(context);
-//            root.addPreResources(new DirResourceSet(root, "/WEB-INF/classes", webInfoClasses.getAbsolutePath(), "/"));
-//            context.setResources(root);
-//            tomcat.addWebapp("/", System.getProperty("user.dir") + File.separator + root);
-
-            tomcat.start();
-            tomcat.getServer().await();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    /**
+     * 设置workspace
+     * @return
+     * @throws IOException
+     */
+    private String getWorkspace() throws IOException {
+        File tempFile = File.createTempFile("tomcat.", "." + builder.port);
+        tempFile.delete();
+        tempFile.mkdir();
+        tempFile.deleteOnExit();
+        return tempFile.getAbsolutePath();
     }
 
     public static WebService webService() {
-        return new WebService();
+        return new WebService.Builder().build();
     }
 
 }
