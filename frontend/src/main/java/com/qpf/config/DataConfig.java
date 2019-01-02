e8f017abafd32b2e4a8d145dd9f5aa624728b342
shadaileng <qpf0510@qq.com>
2019-01-02 15:27:20 +0800

    modufy

--- frontend/src/main/java/com/qpf/config/DataConfig.java
+++ frontend/src/main/java/com/qpf/config/DataConfig.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -12,17 +13,26 @@ import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 @Configuration
-@PropertySource("classpath:application.properties")
+@PropertySource("classpath:/application.properties")
 @MapperScan("com.qpf.dao")
 public class DataConfig {
+	@Value("${jdbc.url}")
+	private String jdbcUrl;
+	@Value("${jdbc.driver}")
+	private String jdbcDriver;
+	@Value("${jdbc.user}")
+	private String user;
+	@Value("${jdbc.password}")
+	private String password;
 	@Bean
-	public DataSource  dataSource() throws Exception {
+	public DataSource dataSource() throws Exception {
 		ComboPooledDataSource dataSource = new ComboPooledDataSource();
-		dataSource.setDriverClass("org.sqlite.JDBC");
-		dataSource.setJdbcUrl("jdbc:sqlite:F:/qipf/tmp/data.db");
+		dataSource.setDriverClass(jdbcDriver);
+		dataSource.setJdbcUrl(jdbcUrl);
+		dataSource.setUser(user);
+		dataSource.setPassword(password);
+//		dataSource.setJdbcUrl("jdbc:sqlite:F:/qipf/tmp/data.db");
 //		dataSource.setJdbcUrl("jdbc:sqlite::resource:data/data.db");
-		dataSource.setUser("");
-		dataSource.setPassword("");
 		return dataSource;
 	}
 	@Bean
